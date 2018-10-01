81547cf9615219e1a85c49b3a41a006c13668ee0
Andy Steere <18317106+awsteere@users.noreply.github.com>
2018-10-01 13:57:58 -0600

    Attempt at getting the tests to run

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to githupapp!');
+    expect(page.getParagraphText()).toEqual('Angular Issues From The Last 7 Days');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { IssuesComponent } from './issues/issues.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        IssuesComponent
       ],
     }).compileComponents();
   }));
@@ -13,15 +16,16 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'githupapp'`, async(() => {
+  it(`should have as title 'Angular Issues'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('githupapp');
+    expect(app.title).toEqual('Angular Issues');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to githupapp!');
+    const expectedTitle = 'Angular Issues From The Last 7 Days';
+    expect(compiled.querySelector('h1').textContent).toContain(expectedTitle);
   }));
 });
